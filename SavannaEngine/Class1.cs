5cf8da8c150cf5b2611b8b50f6c94737bfb48b9f
sabine.petrovska <sabine.petrovska@LVAGPLTP7215>
2025-09-02 12:03:45 +0300

    Add Antelope and Lion classes to backend and create GameEngine for animal management

--- SavannaEngine/Class1.cs
+++ SavannaEngine/Class1.cs
@@ -1,7 +1,27 @@
 ﻿namespace SavannaEngine
 {
-    public class Class1
+    public abstract class Animal
     {
+        public string Name { get; set; }
+    }
 
+    public class Antelope : Animal
+    {
+        public Antelope() => Name = "Antelope";
+    }
+
+    public class Lion : Animal
+    {
+        public Lion() => Name = "Lion";
+    }
+
+    public class GameEngine
+    {
+        public List<Animal> Animals { get; } = new();
+
+        public void AddAnimal(Animal animal)
+        {
+            Animals.Add(animal);
+        }
     }
 }
