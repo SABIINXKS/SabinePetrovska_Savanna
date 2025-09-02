eb9778dadc33ef89e91179e5df152057b7f5c64a
sabine.petrovska <sabine.petrovska@LVAGPLTP7215>
2025-09-02 11:21:48 +0300

    Implement key press logic to add Antelope and Lion to the game field

--- SavannaGame/Program.cs
+++ SavannaGame/Program.cs
@@ -1,1 +1,34 @@
-﻿// test
+﻿using SavannaEngine;
+
+class Program
+{
+    static void Main()
+    {
+        var engine = new GameEngine();
+
+        Console.WriteLine("Press 'A' to add Antelope, 'L' to add Lion, 'Q' to quit. ");
+
+        while (true)
+        {
+            var key = Console.ReadKey(true).Key;
+
+            if (key == ConsoleKey.A)
+            {
+                engine.AddAnimal(new Antelope());
+                Console.WriteLine("Antelope added.");
+            }
+            else if (key == ConsoleKey.L)
+            {
+                engine.AddAnimal(new Lion());
+                Console.WriteLine("Lion added.");
+            }
+            else if (key == ConsoleKey.Q)
+            {
+                break;
+            }
+        }
+        Console.WriteLine("Animals on field:");
+        foreach (var animal in engine.Animals)
+            Console.WriteLine(animal.Name);
+    }
+}
