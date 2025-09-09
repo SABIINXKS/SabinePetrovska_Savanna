e05062786c507c56f9191b4ff327c12c9f8ff842
sabine.petrovska <sabine.petrovska@LVAGPLTP7215>
2025-09-09 14:34:32 +0300

    Add XML summary comments to Antelope class and its methods

--- SavannaEngine/Antelope.cs
+++ SavannaEngine/Antelope.cs
@@ -1,7 +1,17 @@
-﻿namespace SavannaEngine
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SavannaEngine
 {
+    /// <summary>
+    /// Represents an antelope with specific movement and behavior.
+    /// </summary>
     public class Antelope : Animal
     {
+        /// <summary>
+        /// Initializes a new instance of the Antelope class with default properties.
+        /// </summary>
         public Antelope()
         {
             Name = "Antelope";
@@ -10,6 +20,12 @@
             ActionInterval = 2; // Acts every 2 ticks
         }
 
+        /// <summary>
+        /// Moves the antelope away from nearby lions or randomly if no lions are nearby.
+        /// </summary>
+        /// <param name="animals">List of all animals in the field.</param>
+        /// <param name="fieldWidth">Width of the field.</param>
+        /// <param name="fieldHeight">Height of the field.</param>
         public override void Move(List<Animal> animals, int fieldWidth, int fieldHeight)
         {
             var nearestLion = animals
@@ -34,6 +50,10 @@
             }
         }
 
+        /// <summary>
+        /// Performs a special action unique to the antelope (placeholder).
+        /// </summary>
+        /// <param name="animals">List of all animals in the field.</param>
         public override void PerformSpecialAction(List<Animal> animals)
         {
             // Example: Antelope could "jump" or "hide" (not implemented, placeholder)
