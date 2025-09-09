2debff5624c855e3b0602f21b1e957ecc7e11743
sabine.petrovska <sabine.petrovska@LVAGPLTP7215>
2025-09-09 14:23:13 +0300

    Add XML summary comments to Animal class and its methods

--- SavannaEngine/Animal.cs
+++ SavannaEngine/Animal.cs
@@ -1,16 +1,48 @@
 ﻿namespace SavannaEngine
 {
+    /// <summary>
+    /// Represents a base animal with movement and action logic.
+    /// </summary>
     public abstract class Animal
     {
+        /// <summary>
+        /// Gets or sets the name of the animal.
+        /// </summary>
         public string Name { get; set; }
-        public int X { get; set; }
-        public int Y { get; set; }
-        public int Speed { get; set; }
-        public int VisionRange { get; set; }
-        public int ActionInterval { get; set; } // How many ticks between actions
-        private int _actionCounter = 0;         // Internal tick counter
 
-        // Called by GameEngine each tick
+        /// <summary>
+        /// Gets or sets the X position of the animal.
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Y position of the animal.
+        /// </summary>
+        public int Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets the movement speed of the animal.
+        /// </summary>
+        public int Speed { get; set; }
+
+        /// <summary>
+        /// Gets or sets the vision range of the animal.
+        /// </summary>
+        public int VisionRange { get; set; }
+
+        /// <summary>
+        /// Gets or sets the interval (in ticks) between actions.
+        /// </summary>
+        public int ActionInterval { get; set; }
+
+        private int _actionCounter = 0; // Internal tick counter
+
+        /// <summary>
+        /// Advances the animal's internal tick counter and triggers actions at the specified interval.
+        /// </summary>
+        /// <param name="animals">List of all animals in the field.</param>
+        /// <param name="fieldWidth">Width of the field.</param>
+        /// <param name="fieldHeight">Height of the field.</param>
         public void Tick(List<Animal> animals, int fieldWidth, int fieldHeight)
         {
             _actionCounter++;
@@ -22,8 +54,25 @@
             }
         }
 
+        /// <summary>
+        /// Moves the animal according to its logic.
+        /// </summary>
+        /// <param name="animals">List of all animals in the field.</param>
+        /// <param name="fieldWidth">Width of the field.</param>
+        /// <param name="fieldHeight">Height of the field.</param>
         public abstract void Move(List<Animal> animals, int fieldWidth, int fieldHeight);
+
+        /// <summary>
+        /// Performs a special action unique to the animal type.
+        /// </summary>
+        /// <param name="animals">List of all animals in the field.</param>
         public virtual void PerformSpecialAction(List<Animal> animals) { }
+
+        /// <summary>
+        /// Calculates the Euclidean distance to another animal.
+        /// </summary>
+        /// <param name="other">The other animal.</param>
+        /// <returns>Distance as a double.</returns>
         protected double DistanceTo(Animal other) =>
             Math.Sqrt(Math.Pow(X - other.X, 2) + Math.Pow(Y - other.Y, 2));
     }
