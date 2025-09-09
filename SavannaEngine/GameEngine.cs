88ac6187650a0149f82dc1ef73d2f0392c63c249
sabine.petrovska <sabine.petrovska@LVAGPLTP7215>
2025-09-09 14:07:17 +0300

    Add GameEngine class to manage animals and game ticks

--- SavannaEngine/GameEngine.cs
+++ SavannaEngine/GameEngine.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SavannaEngine
+﻿namespace SavannaEngine
 {
     public class GameEngine
     {
+        public List<Animal> Animals { get; } = new();
+        public int FieldWidth { get; }
+        public int FieldHeight { get; }
+
+        public GameEngine(int fieldWidth = 20, int fieldHeight = 10)
+        {
+            FieldWidth = fieldWidth;
+            FieldHeight = fieldHeight;
+        }
+
+        public void AddAnimal(Animal animal)
+        {
+            Random rnd = new();
+            animal.X = rnd.Next(0, FieldWidth);
+            animal.Y = rnd.Next(0, FieldHeight);
+            Animals.Add(animal);
+        }
+
+        public void Tick()
+        {
+            var currentAnimals = Animals.ToList();
+            foreach (var animal in currentAnimals)
+                animal.Tick(Animals, FieldWidth, FieldHeight);
+        }
     }
 }
