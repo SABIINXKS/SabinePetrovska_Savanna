7ab3c9f92ccec1b43d79f7dc03b76e35b5bba98c
sabine.petrovska <sabine.petrovska@LVAGPLTP7215>
2025-09-09 14:03:07 +0300

    Add abstract Animal base class with movement and action logic

--- SavannaEngine/Animal.cs
+++ SavannaEngine/Animal.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SavannaEngine
+﻿namespace SavannaEngine
 {
-    public class Animal
+    public abstract class Animal
     {
+        public string Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Speed { get; set; }
+        public int VisionRange { get; set; }
+        public int ActionInterval { get; set; } // How many ticks between actions
+        private int _actionCounter = 0;         // Internal tick counter
+
+        // Called by GameEngine each tick
+        public void Tick(List<Animal> animals, int fieldWidth, int fieldHeight)
+        {
+            _actionCounter++;
+            if (_actionCounter >= ActionInterval)
+            {
+                Move(animals, fieldWidth, fieldHeight);
+                PerformSpecialAction(animals);
+                _actionCounter = 0;
+            }
+        }
+
+        public abstract void Move(List<Animal> animals, int fieldWidth, int fieldHeight);
+        public virtual void PerformSpecialAction(List<Animal> animals) { }
+        protected double DistanceTo(Animal other) =>
+            Math.Sqrt(Math.Pow(X - other.X, 2) + Math.Pow(Y - other.Y, 2));
     }
 }
