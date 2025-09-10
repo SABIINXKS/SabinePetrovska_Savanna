9974dfa441ffe32376d47974744a7e3af4d84182
sabine.petrovska <sabine.petrovska@LVAGPLTP7215>
2025-09-10 09:05:58 +0300

    Add SavannaConstants class for default field size constants

---
+++ SavannaEngine/SavannaConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace SavannaEngine
+{
+    /// <summary>
+    /// Contains constant values used throughout the Savanna simulation.
+    /// </summary>
+    public static class SavannaConstants
+    {
+        public const int DefaultFieldWidth = 20;
+        public const int DefaultFieldHeight = 10;
+    }
+}
