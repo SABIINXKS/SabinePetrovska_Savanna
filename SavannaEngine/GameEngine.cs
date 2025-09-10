bda847cb6a7e0e265f1b5f993c13e78434e7ca5c
sabine.petrovska <sabine.petrovska@LVAGPLTP7215>
2025-09-10 09:02:51 +0300

    Add XML summary comments to GameEngine class and its methods

--- SavannaEngine/GameEngine.cs
+++ SavannaEngine/GameEngine.cs
@@ -1,17 +1,44 @@
-﻿namespace SavannaEngine
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SavannaEngine
 {
+    /// <summary>
+    /// Manages the game state, animals, and game ticks.
+    /// </summary>
     public class GameEngine
     {
+        /// <summary>
+        /// Gets the list of animals in the game.
+        /// </summary>
         public List<Animal> Animals { get; } = new();
+
+        /// <summary>
+        /// Gets the width of the field.
+        /// </summary>
         public int FieldWidth { get; }
+
+        /// <summary>
+        /// Gets the height of the field.
+        /// </summary>
         public int FieldHeight { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the GameEngine class with specified field dimensions.
+        /// </summary>
+        /// <param name="fieldWidth">Width of the field.</param>
+        /// <param name="fieldHeight">Height of the field.</param>
         public GameEngine(int fieldWidth = 20, int fieldHeight = 10)
         {
             FieldWidth = fieldWidth;
             FieldHeight = fieldHeight;
         }
 
+        /// <summary>
+        /// Adds an animal to the game at a random position.
+        /// </summary>
+        /// <param name="animal">The animal to add.</param>
         public void AddAnimal(Animal animal)
         {
             Random rnd = new();
@@ -20,6 +47,9 @@
             Animals.Add(animal);
         }
 
+        /// <summary>
+        /// Advances the game by one tick, updating all animals.
+        /// </summary>
         public void Tick()
         {
             var currentAnimals = Animals.ToList();
