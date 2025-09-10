094aa47e64165a26c91335e99a9dc92b96f8fdcb
sabine.petrovska <sabine.petrovska@LVAGPLTP7215>
2025-09-10 09:37:12 +0300

    Add all display and control literals to SavannaConstants class

--- SavannaEngine/SavannaConstants.cs
+++ SavannaEngine/SavannaConstants.cs
@@ -7,5 +7,16 @@
     {
         public const int DefaultFieldWidth = 20;
         public const int DefaultFieldHeight = 10;
+        public const int LoopDelayMs = 500;
+        public const string AddAntelopeKey = "A";
+        public const string AddLionKey = "L";
+        public const string QuitKey = "Q";
+        public const char LionSymbol = 'L';
+        public const char AntelopeSymbol = 'A';
+        public const char EmptyCellSymbol = ' ';
+        public const char BorderSymbol = '#';
+        public const string AddAnimalPrompt = "Press 'A' to add Antelope, 'L' to add Lion, 'Q' to quit.";
+        public const string FieldLabel = "Field:";
+        public const string AnimalsLabel = "Animals on field:";
     }
 }
